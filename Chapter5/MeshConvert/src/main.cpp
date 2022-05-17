1f26e2622d6abdea8b0e0427e500d10f887ff41c
Zixin Zhang <zzhng151@illinois.edu>
2022-05-17 14:31:28 -0400

    Add more comments

--- Chapter5/MeshConvert/src/main.cpp
+++ Chapter5/MeshConvert/src/main.cpp
@@ -6,6 +6,9 @@
 
 #include <meshoptimizer.h>
 
+// This mesh conversion tool preprocess a mesh so that we can store it in a runtime efficient data format
+
+
 // TODO: There is no global verbose flag as mentioned at page 246
 
 MeshData g_meshData;
@@ -29,10 +32,18 @@ float g_meshScale = 0.01f;
 // By default, we don't calculate LODs
 bool g_calculateLODs = false;
 
+/**
+ * \brief Create LOD indices
+ * \param indices The original indices
+ * \param vertices The original vertices
+ * \param outLods The output collection of indices that represent LOD meshes
+ */
 void processLods(std::vector<uint32_t>& indices,
                  std::vector<float>& vertices,
                  std::vector<std::vector<uint32_t>>& outLods)
 {
+	// Each vertex is constructed from 3 float values
+	// TODO: in the book, size_t verticesCountIn = vertices.size() / 3;
 	size_t verticesCountIn = vertices.size() / 2;
 	size_t targetIndicesCount = indices.size();
 
@@ -48,12 +59,14 @@ void processLods(std::vector<uint32_t>& indices,
 
 		bool sloppy = false;
 
-		size_t numOptIndices = meshopt_simplify(
-			indices.data(),
-			indices.data(), (uint32_t)indices.size(),
-			vertices.data(), verticesCountIn,
-			sizeof(float) * 3,
-			targetIndicesCount, 0.02f);
+		size_t numOptIndices = meshopt_simplify(indices.data(),
+		                                        indices.data(),
+		                                        (uint32_t)indices.size(),
+		                                        vertices.data(),
+		                                        verticesCountIn,
+		                                        sizeof(float) * 3,
+		                                        targetIndicesCount,
+		                                        0.02f);
 
 		// cannot simplify further
 		if (static_cast<size_t>(numOptIndices * 1.1f) > indices.size())
@@ -86,21 +99,31 @@ void processLods(std::vector<uint32_t>& indices,
 	}
 }
 
+/**
+ * \brief This function converts an input assimp mesh into a mesh of our own representation
+ * \param m The input Assimp mesh
+ * \return The mesh of our own representation
+ */
 Mesh convertAIMesh(const aiMesh* m)
 {
+	// check whether a set of texture coordinates is present in the original Assimp mesh
 	const bool hasTexCoords = m->HasTextureCoords(0);
+	// The size of the stream element in bytes is directly calculated from the number of elements per vertex
 	const uint32_t streamElementSize = static_cast<uint32_t>(g_numElementsToStore * sizeof(float));
 
 	// Original data for LOD calculation
 	std::vector<float> srcVertices;
 	std::vector<uint32_t> srcIndices;
 
+	// if we don't have LOD, the first element in this vector is the original data indices
 	std::vector<std::vector<uint32_t>> outLods;
 
 	auto& vertices = g_meshData.vertexData_;
 
+	// For each of the vertices, we extract their data from the aiMesh object
 	for (size_t i = 0; i != m->mNumVertices; i++)
 	{
+		// we export vertex, normal, and texture coordinate
 		const aiVector3D v = m->mVertices[i];
 		const aiVector3D n = m->mNormals[i];
 		const aiVector3D t = hasTexCoords ? m->mTextureCoords[0][i] : aiVector3D();
@@ -112,12 +135,13 @@ Mesh convertAIMesh(const aiMesh* m)
 			srcVertices.push_back(v.z);
 		}
 
+		// append vertex, texture coordinate, and normal to the vertex stream
 		vertices.push_back(v.x * g_meshScale);
 		vertices.push_back(v.y * g_meshScale);
 		vertices.push_back(v.z * g_meshScale);
 
 		vertices.push_back(t.x);
-		vertices.push_back(1.0f - t.y);
+		vertices.push_back(1.0f - t.y); // note: y-coordinate is flipped
 
 		vertices.push_back(n.x);
 		vertices.push_back(n.y);
@@ -125,52 +149,78 @@ Mesh convertAIMesh(const aiMesh* m)
 	}
 
 	Mesh result = {
+		// we only have 1 vertex stream per mesh
 		.streamCount = 1,
+		// set the index and vertex offset within the input file
 		.indexOffset = g_indexOffset,
 		.vertexOffset = g_vertexOffset,
+		// vertex count is the number of vertices in this mesh
 		.vertexCount = m->mNumVertices,
+		// update stream offset and element size for this mesh
 		.streamOffset = {g_vertexOffset * streamElementSize},
 		.streamElementSize = {streamElementSize}
 	};
 
 	for (size_t i = 0; i != m->mNumFaces; i++)
 	{
+		// skip non-triangle faces
 		if (m->mFaces[i].mNumIndices != 3)
 			continue;
+		// populate data indices
 		for (unsigned j = 0; j != m->mFaces[i].mNumIndices; j++)
+		{
 			srcIndices.push_back(m->mFaces[i].mIndices[j]);
+		}
 	}
 
 	if (!g_calculateLODs)
+	{
 		outLods.push_back(srcIndices);
+	}
 	else
+	{
 		processLods(srcIndices, srcVertices, outLods);
+	}
 
 	printf("\nCalculated LOD count: %u\n", (unsigned)outLods.size());
 
+	// put LOD indices into Mesh's indices
 	uint32_t numIndices = 0;
 	for (size_t l = 0; l < outLods.size(); l++)
 	{
 		for (size_t i = 0; i < outLods[l].size(); i++)
+		{
 			g_meshData.indexData_.push_back(outLods[l][i]);
+		}
 
 		result.lodOffset[l] = numIndices;
 		numIndices += (int)outLods[l].size();
 	}
 
+	// last item of loadOffset array is used for special purpose
 	result.lodOffset[outLods.size()] = numIndices;
+
 	result.lodCount = (uint32_t)outLods.size();
 
+	// After processing the input mesh, we increment offset counters for the indices and current starting vertex
 	g_indexOffset += numIndices;
 	g_vertexOffset += m->mNumVertices;
 
 	return result;
 }
 
+/**
+ * \brief This function processes the input file.
+ * This includes loading the scene and converting each mesh into an internal format (our own mesh representation)
+ * \param fileName The input file name
+ */
 void loadFile(const char* fileName)
 {
 	printf("Loading '%s'...\n", fileName);
 
+	// The list of flags for the ASSIMP import function
+	// TODO: aiProcess_PreTransformVertices, aiProcess_FindInstances, aiProcess_OptimizeMeshes are provided in the book, but are here
+	// TODO: aiProcess_LimitBoneWeights, aiProcess_SplitLargeMeshes, aiProcess_ImproveCacheLocality are provided here, but not in the book
 	const unsigned int flags = 0 |
 		aiProcess_JoinIdenticalVertices |
 		aiProcess_Triangulate |
@@ -191,9 +241,11 @@ void loadFile(const char* fileName)
 		exit(255);
 	}
 
+	// After importing the scene, we reserve the memory for the mesh descriptor and bounding box containers accordingly 
 	g_meshData.meshes_.reserve(scene->mNumMeshes);
 	g_meshData.boxes_.reserve(scene->mNumMeshes);
 
+	// call convertAIMesh for each mesh in the scene
 	for (unsigned int i = 0; i != scene->mNumMeshes; i++)
 	{
 		printf("\nConverting meshes %u/%u...", i + 1, scene->mNumMeshes);
@@ -201,6 +253,7 @@ void loadFile(const char* fileName)
 		g_meshData.meshes_.push_back(convertAIMesh(scene->mMeshes[i]));
 	}
 
+	// TODO: ignore this in this chapter for now
 	recalculateBoundingBoxes(g_meshData);
 }
 

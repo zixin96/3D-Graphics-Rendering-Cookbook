e6272ce37af47190c9629c2d35744cdb17dc6264
Zixin Zhang <zzhng151@illinois.edu>
2022-05-18 14:58:02 -0400

    add comment

--- Chapter2/01_GLFW/src/main.cpp
+++ Chapter2/01_GLFW/src/main.cpp
@@ -4,6 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//  Let's draw a colored triangle
+// For the sake of brevity, all error checking is omitted 
+
+// We use the GLSL built-in gl_VertexID input variable to index into the pos[]
+// and col[] arrays to generate the vertex positions and colors programmatically
+// In this case, no user-defined inputs to the vertex shader are required.
 static const char* shaderCodeVertex = R"(
 #version 460 core
 layout (location=0) out vec3 color;
@@ -34,80 +41,106 @@ void main()
 };
 )";
 
-int main( void )
+int main(void)
 {
+	// we set the GLFW error callback via a simple lambda to catch potential errors
 	glfwSetErrorCallback(
-		[]( int error, const char* description )
+		[](int error, const char* description)
 		{
-			fprintf( stderr, "Error: %s\n", description );
+			fprintf(stderr, "Error: %s\n", description);
 		}
 	);
 
-	if ( !glfwInit() )
-		exit( EXIT_FAILURE );
+	//  try to initialize GLFW
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
 
-	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
-	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 6 );
-	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+	// tell GLFW which version of OpenGL we want to use
+	// we will use OpenGL 4.6 Core Profile
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow( 1024, 768, "Simple example", nullptr, nullptr );
-	if ( !window )
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "Simple example", nullptr, nullptr);
+	if (!window)
 	{
 		glfwTerminate();
-		exit( EXIT_FAILURE );
+		exit(EXIT_FAILURE);
 	}
 
+	//  set a callback for key events
 	glfwSetKeyCallback(
 		window,
-		[]( GLFWwindow* window, int key, int scancode, int action, int mods )
+		[](GLFWwindow* window, int key, int scancode, int action, int mods)
 		{
-			if ( key == GLFW_KEY_ESCAPE && action == GLFW_PRESS )
-				glfwSetWindowShouldClose( window, GLFW_TRUE );
+			if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+				glfwSetWindowShouldClose(window, GLFW_TRUE);
 		}
 	);
 
-	glfwMakeContextCurrent( window );
-	gladLoadGL( glfwGetProcAddress );
-	glfwSwapInterval( 1 );
+	// prepare the OpenGL context
+	glfwMakeContextCurrent(window);
+	//  use the GLAD library to import all OpenGL entry pointsand extensions
+	gladLoadGL(glfwGetProcAddress);
+	glfwSwapInterval(1);
 
-	const GLuint shaderVertex = glCreateShader( GL_VERTEX_SHADER );
-	glShaderSource( shaderVertex, 1, &shaderCodeVertex, nullptr );
-	glCompileShader( shaderVertex );
+	// Both shaders should be compiled and linked to a shader program
+	const GLuint shaderVertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(shaderVertex, 1, &shaderCodeVertex, nullptr);
+	glCompileShader(shaderVertex);
 
-	const GLuint shaderFragment = glCreateShader( GL_FRAGMENT_SHADER );
-	glShaderSource( shaderFragment, 1, &shaderCodeFragment, nullptr );
-	glCompileShader( shaderFragment );
+	const GLuint shaderFragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(shaderFragment, 1, &shaderCodeFragment, nullptr);
+	glCompileShader(shaderFragment);
 
 	const GLuint program = glCreateProgram();
-	glAttachShader( program, shaderVertex );
-	glAttachShader( program, shaderFragment );
+	glAttachShader(program, shaderVertex);
+	glAttachShader(program, shaderFragment);
 
-	glLinkProgram( program );
-	glUseProgram( program );
+	glLinkProgram(program);
+	glUseProgram(program);
 
+	// create a VAO
+	// For this example, we will use the vertex shader to generate
+	// all vertex data, so an empty VAO will be sufficient
 	GLuint vao;
-	glCreateVertexArrays( 1, &vao );
-	glBindVertexArray( vao );
+	glCreateVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
-	glClearColor( 1.0f, 1.0f, 1.0f, 1.0f );
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-	while ( !glfwWindowShouldClose( window ) )
+	// The main loop starts by checking whether the window should be closed
+	while (!glfwWindowShouldClose(window))
 	{
+		// Implement a resizable window by reading the current width and height from GLFW
+		// and updating the OpenGL viewport accordingly
 		int width, height;
-		glfwGetFramebufferSize( window, &width, &height );
-		glViewport( 0, 0, width, height );
-		glClear( GL_COLOR_BUFFER_BIT );
-		glDrawArrays( GL_TRIANGLES, 0, 3 );
-		glfwSwapBuffers( window );
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+
+		// Clear the screen
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// render the triangle
+		// The glDrawArrays() function can be invoked with the empty VAO that we bound earlier
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// The fragment shader output was rendered into the back buffer. Let's swap the front
+		// and back buffers to make the triangle visible
+		glfwSwapBuffers(window);
+
+		// poll the events
 		glfwPollEvents();
 	}
 
-	glDeleteProgram( program );
-	glDeleteShader( shaderFragment );
-	glDeleteShader( shaderVertex );
-	glDeleteVertexArrays( 1, &vao );
+	//  delete the OpenGL objects that we created
+	glDeleteProgram(program);
+	glDeleteShader(shaderFragment);
+	glDeleteShader(shaderVertex);
+	glDeleteVertexArrays(1, &vao);
 
-	glfwDestroyWindow( window );
+	// terminate GLFW
+	glfwDestroyWindow(window);
 	glfwTerminate();
 
 	return 0;
